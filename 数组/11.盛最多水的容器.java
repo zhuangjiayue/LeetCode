9e3ac6cc3639c9fa40de1384befe0573727ce769
zhuangjiayue <32437001+zhuangjiayue@users.noreply.github.com>
2019-12-20 22:37:27 +0800

    Update 11.盛最多水的容器.java

--- 数组/11.盛最多水的容器.java
+++ 数组/11.盛最多水的容器.java
@@ -33,7 +33,7 @@ class Solution {
         // 2. 将计算出来的面积值与当前面积比较
         // int maxArea = 0;
         // for (int i = 0; i < height.length - 1; i++) {
-        //     for (int j = i; j < height.length; j++) {
+        //     for (int j = i + 1; j < height.length; j++) {
         //         maxArea = Math.max(maxArea, (j - i) * Math.min(height[i], height[j]));
         //     }
         // }
